1ad55affca42473bbd9fd97348a3e8374a90f1ea
Avrodip <avrodp25@gmail.com>
2025-06-04 08:36:05 +0530

    Company controller created and login routes created

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,4 +1,4 @@
-import { User } from "../models/user.model.js";
+import { User } from "../models/user.models.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
@@ -93,3 +93,68 @@ export const login = async(req,res) =>{
     }
 }
 
+export const logout = async(req,res) => {
+    try {
+        return res.status(200).cookie("token","",{maxAge:0}).json({
+            message:"Logged out successfully",
+            success:true
+        })
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
+
+export const updateProfile = async(req,res) => {
+    try {
+        const {fullname,email,phoneNumber,bio,skills} = req.body;
+        const file = req.file;
+        // if(!fullname || !email || !phoneNumber || !bio || !skills ){
+        //     return res.status(400).json({
+        //         message:"Something is missing",
+        //         success:false
+        //     });
+        // };
+
+        //cloudinary pending
+        let skillsArray;
+        if(skills){
+         skillsArray = skills.split(",");
+        }
+        const userId = req.id 
+        let user = await User.findById(userId);
+
+        if(!user){
+            return res.status(400).json({
+                message:"User not found",
+                success:false
+            })
+        }
+        //updating data
+        if(fullname) user.fullname = fullname
+        if(email) user.email = email
+        if(phoneNumber) user.phoneNumber = phoneNumber
+        if(bio) user.profile.bio = bio
+        if(skills) user.profile.skills = skillsArray
+
+        await user.save();
+
+        user = {
+            _id:user._id,
+            fullname: user.fullname,
+            email: user.email,
+            phoneNumber: user.phoneNumber,
+            role:user.role,
+            profile:user.profile
+        }
+        return res.status(200).json({
+            message:"Profile updated successfully.",
+            user,
+            success:true
+        })
+
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
